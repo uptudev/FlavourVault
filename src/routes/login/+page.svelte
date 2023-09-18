71d4d1e765f38009b722d83cf39406264213f278
uptu <91502176+uptudev@users.noreply.github.com>
2023-09-18 03:36:05 -0600

    Front End prototype & basic data structuring (#1)
    * Front End prototype & basic data structuring
    
    * Update firebase-hosting-pull-request.yml

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,41 @@
+<script lang='ts'>
+    import { writable } from 'svelte/store';
+    import { FVUser } from '../UserData.ts';
+    import { auth, handleSignUp, handleSignIn } from '../../firebase.ts';
+    
+    let user = null;
+
+    function setUser(email: string, password: string) {
+        
+    }
+
+</script>
+
+<style lang='scss'>
+
+</style>
+
+{#if (user != null)}
+
+{:else}
+<h2>Log In</h2>
+    <form>
+        <label class="login_title">
+            Email:
+        </label><br />
+        <input class="login" type="text" id="email" name="email" />
+        <br />
+        <label class="login_title">
+            Password:
+        </label><br />
+        <input class="login" type="password" id="password" name="password" />
+        <div>
+            <button class="login-button" type="submit" onClick={handleSignIn}>
+                Sign in
+            </button>
+            <button class="login-button" type="submit" onClick={handleSignUp}>
+                Sign up
+            </button>
+        </div>
+    </form>
+{/if}
